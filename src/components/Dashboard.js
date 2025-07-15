ea6ecd8c5b4b54fa5efd630c425da3894ad58217
Sudhir Rathod <rathodsudhir326@gmail.com>
2025-07-15 20:44:15 +0530

    h

---
+++ src/components/Dashboard.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
+// import "./dashboard.css"
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <GeneralContextProvider>
+        <WatchList />
+      </GeneralContextProvider>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
+// const Dashboard = () => {
+//   return (
+//     <div className="dashboard-container">
+//       <div className="watchlist-container">
+//         <GeneralContextProvider>
+//           <WatchList />
+//         </GeneralContextProvider>
+//       </div>
+//       <div className="content">
+//         <Routes>
+//           <Route exact path="/" element={<Summary />} />
+//           <Route path="/orders" element={<Orders />} />
+//           <Route path="/holdings" element={<Holdings />} />
+//           <Route path="/positions" element={<Positions />} />
+//           <Route path="/funds" element={<Funds />} />
+//           <Route path="/apps" element={<Apps />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
---
+++ src/components/Orders.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Orders = () => {
+  const [allOrders, setAllorders] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrders").then((res) => {
+      console.log(res.data);
+      setAllorders(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="orders">
+      <div className="no-orders">
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+    
+        <div className="order-table" style={{margin:"20px"}}>
+        {allOrders.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Mode</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allOrders.map((order, index) => (
+                <tr key={index}>
+                  <td>{order.name}</td>
+                  <td>{order.qty}</td>
+                  <td>{order.price}</td>
+                  <td>{order.mode}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
