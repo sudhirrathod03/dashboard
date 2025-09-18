b205da67993c426381cbaa81c018ddba0ad893fc
Sudhir Rathod <rathodsudhir326@gmail.com>
2025-09-18 22:05:36 +0530

    fixed

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./order.css"
 import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_URL;
 const Orders = () => {
